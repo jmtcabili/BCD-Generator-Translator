4e2104f4ca129445096edec8cdb4434601078e3a
Aebrahm Ramos <aebrahmramos@proton.me>
2024-07-17 13:29:14 +0800

    packed and unpacked bcd

---
+++ index.js
@@ -0,0 +1,35 @@
+function fromDecimal() {
+    const binaryMap = {
+        '0': '0000',
+        '1': '0001',
+        '2': '0010',
+        '3': '0011',
+        '4': '0100',
+        '5': '0101',
+        '6': '0110',
+        '7': '0111',
+        '8': '1000',
+        '9': '1001'
+    };
+    const decimalInput = document.getElementById('decimalInput').value.split('');
+    let unpackedOutput = document.getElementById('uBCD');
+    let packedOutput = document.getElementById('pBCD');
+    let denseOutput = document.getElementById('dBCD');
+
+    let unpackedResult = unpackedBCD(binaryMap, decimalInput);
+    let packedResult = packedBCD(binaryMap, decimalInput);
+    let denseResult = denseBCD(binaryMap, decimalInput);
+
+    unpackedOutput.innerHTML = unpackedResult;
+    packedOutput.innerHTML = packedResult;
+    denseOutput.innerHTML = denseResult;
+}
+
+function unpackedBCD(binaryMap, decimalInput) {
+    return decimalInput.map(digit => '0000-' + binaryMap[digit]).join(' ');
+}
+
+function packedBCD(binaryMap, decimalInput) {
+    return decimalInput.map(digit => binaryMap[digit]).join(' ');
+}
+
