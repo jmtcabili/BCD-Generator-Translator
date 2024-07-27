7d9c400a787640255de3a6769239453076c3e804
Johan Marlo Cabili <jmtcabili@gmail.com>
2024-07-27 13:36:52 +0800

    restored old js

--- index.js
+++ index.js
@@ -26,15 +26,124 @@ function fromDecimal() {
 }
 
 function unpackedBCD(binaryMap, decimalInput) {
-    return decimalInput.map(digit => '0000' + binaryMap[digit]).join(' ');
+    return decimalInput.map(digit => '0000-' + binaryMap[digit]).join(' ');
 }
 
 function packedBCD(binaryMap, decimalInput) {
     return decimalInput.map(digit => binaryMap[digit]).join(' ');
 }
 
-function denseBCD(binaryMap, decimalInput) {
-    return "a;sdlkfja"   
+function densePackedBCD(binaryMap, decimalInput) {
+    let densePackedBCD = [];
+    const arr = packedResult.split('');
+    int a = arr[0];
+    int b = arr[1];
+    int c = arr[2];
+    int d = arr[3];
+    int e = arr[4];
+    int f = arr[5];
+    int g = arr[6];
+    int h = arr[7];
+    int i = arr[8];
+    int j = arr[9];
+    int k = arr[10];
+    int m = arr[11];
+
+    densePackedBCD.push(a);
+    densePackedBCD.push(e);
+    densePackedBCD.push(i);
+
+    if (a == 0){ 
+        densePackedBCD.push(b);
+        densePackedBCD.push(c);
+        densePackedBCD.push(d);
+        if (e == 0){
+            densePackedBCD.push(f);
+            densePackedBCD.push(g);
+            densePackedBCD.push(h);
+            if (i == 0){
+                densePackedBCD.push(0);
+                densePackedBCD.push(j);
+                densePackedBCD.push(k);
+                //aei = 000
+            }else{
+                densePackedBCD.push(1);
+                densePackedBCD.push(0);
+                densePackedBCD.push(0);
+                //aei = 001
+            }
+        }else{
+            if (i == 0){
+                densePackedBCD.push(j);
+                densePackedBCD.push(k);
+                densePackedBCD.push(h);
+                densePackedBCD.push(1);
+                densePackedBCD.push(0);
+                densePackedBCD.push(1);
+                //aei = 010
+            }else{
+                densePackedBCD.push(1);
+                densePackedBCD.push(0);
+                densePackedBCD.push(h);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                //aei = 011
+            }
+        }
+    }else{
+        if (e == 0){
+            if (i == 0){
+                densePackedBCD.push(j);
+                densePackedBCD.push(k);
+                densePackedBCD.push(d);
+                densePackedBCD.push(f);
+                densePackedBCD.push(g);
+                densePackedBCD.push(h);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                densePackedBCD.push(0);
+                //aei = 100
+            }else{
+                densePackedBCD.push(f);
+                densePackedBCD.push(g);
+                densePackedBCD.push(d);
+                densePackedBCD.push(0);
+                densePackedBCD.push(1);
+                densePackedBCD.push(h);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                //aei = 101
+            }
+        }else{
+            if (i == 0){
+                densePackedBCD.push(j);
+                densePackedBCD.push(k);
+                densePackedBCD.push(d);
+                densePackedBCD.push(0);
+                densePackedBCD.push(0);
+                densePackedBCD.push(h);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                //aei = 110
+            }else{
+                densePackedBCD.push(0);
+                densePackedBCD.push(0);
+                densePackedBCD.push(d);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                densePackedBCD.push(h);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                densePackedBCD.push(1);
+                //aei = 111
+            }
+        }
+    }
+    densePackedBCD.push(m);
+    return densePackedBCD.join(' ');
+    
     
 }
-
