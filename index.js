6716e06a07966025270b41f32fba21a33f4009ab
Matthew Chua <matthew_adrian_u_chua@dlsu.edu.ph>
2024-07-30 04:15:18 +0800

    updated densely packed bcd to decimal to accept more than 10 digits in chunks of 10 bits and added conversion to decimal

--- index.js
+++ index.js
@@ -62,9 +62,13 @@ function fromBCD(){
 
      
     //generate default 0-array
-    numBits = numGroups * 12 + numExcess; 
+    numBits = numGroups * 12 + numExcess;
+
+    console.log(numGroups);
 
     let result = new Array(numBits).fill('0')
+    
+    for(let x = 1; x <= numGroups ; x++){
     
         //default values
         result[idx['d']]  = dBCDInput[idx['r']]
@@ -156,7 +160,54 @@ function fromBCD(){
                 }
             }
         }
+
+        //increment the indices of the idx array for the next group
+        idx.a = 0 + (12*x);
+        idx.b = 1 + (12*x);
+        idx.c = 2 + (12*x);
+        idx.d = 3 + (12*x);
+        idx.e = 4 + (12*x);
+        idx.f = 5 + (12*x);
+        idx.g = 6 + (12*x);
+        idx.h = 7 + (12*x);
+        idx.i = 8 + (12*x);
+        idx.j = 9 + (12*x);
+        idx.k = 10 + (12*x);
+        idx.m = 11 + (12*x);
+
+        idx.p = 0 + (10*x);
+        idx.q = 1 + (10*x);
+        idx.r = 2 + (10*x);
+        idx.s = 3 + (10*x);
+        idx.t = 4 + (10*x);
+        idx.u = 5 + (10*x);
+        idx.v = 6 + (10*x);
+        idx.w = 7 + (10*x);
+        idx.x = 8 + (10*x);
+        idx.y = 9 + (10*x);
+
+    }
     
     final = result.join(''); 
-    decimalOutput.innerHTML = final;
+
+    const binaryMap = {
+        '0000': '0',
+        '0001': '1',
+        '0010': '2',
+        '0011': '3',
+        '0100': '4',
+        '0101': '5',
+        '0110': '6',
+        '0111': '7',
+        '1000': '8',
+        '1001': '9'
+    };
+    
+    // Split the joined BCD string into chunks of 4 bits and map each chunk to its decimal equivalent
+    const decimalString = final.match(/.{1,4}/g).map(digit => binaryMap[digit]).join('');
+    
+    // Convert the decimal string to a number to remove the non significant zeroes
+    const decimalOutputNum = parseInt(decimalString, 10);
+
+    decimalOutput.innerHTML = decimalOutputNum;
 }
